6fba47bd105da51832c929cca1a1d03989dcd3ef
santibruno <santib1502@gmail.com>
2022-04-30 17:44:54 -0300

    ProyectoFinal1erEntrega

--- script.js
+++ script.js
@@ -16,10 +16,9 @@ function agregarProduto(productos){
     while (isNaN(stock)){
         stock =  parseInt(prompt("Ingrese el stock de su producto valido"))
     }
-    productos.push (new Producto(nombre.toUpperCase(),tipo,valor,color.toUpperCase(),stock))
+    productos.push (new Producto(nombre,tipo,valor,color,stock))
     return alert (`El Producto ${nombre} fue agregado con exito con un valor de $ ${valor}`)
 }
-
 function agregarStock(productos){
     const nombresProd=productos.map(producto=>producto.nombre)
     while (nombresProd){
@@ -38,13 +37,10 @@ function agregarStock(productos){
         }        
     }
 }
-
-function borrarProducto(productos){
-    
+function borrarProducto(productos){    
     const nombresProd=productos.map(producto=>producto.nombre)
     while (nombresProd){
-        const eleccion = prompt(`Que producto desea eliminar \n ${nombresProd}`)       
-        
+        const eleccion = prompt(`Que producto desea eliminar \n ${nombresProd}`)               
         if (productos.find(producto=>producto.nombre===eleccion.toUpperCase())){
             return productos.filter(producto1=>producto1.nombre!==eleccion.toUpperCase())
         } else {
@@ -52,8 +48,7 @@ function borrarProducto(productos){
         }        
     }
     
-}
-     
+}     
 function disminuirStock(productos){
     const nombresProd=productos.map(producto=>producto.nombre)
     while (nombresProd){
@@ -63,7 +58,7 @@ function disminuirStock(productos){
             console.log(producto)
             let cantidad = parseInt(prompt(`El Stock del producto es ${producto.stock}\n Cuanto desea disminuir?` ))
             while (isNaN(cantidad)||producto.stock<cantidad){
-                cantidad =  parseInt(prompt(`El Stock a disminuir debe ser un valor valido (numerico) \n Cuanto desea agregar?` ))
+                cantidad =  parseInt(prompt(`El Stock a disminuir debe ser un valor valido (numerico) menor a ${producto.stock}\n Cuanto desea eliminar?` ))
             }
             return productos.map(producto1=>{
                 if( producto1.nombre===producto.nombre){producto1.stock-=cantidad}})
@@ -76,38 +71,42 @@ function calcularCuotas(cantidad,valor){
     const impuesto = 0.05*cantidad + 1
     return (valor*impuesto)/cantidad
 }
-function menuEleccion(menu = parseInt(prompt("Ingrese que quiere realizar: \n1:Agregar Producto \n2:Agregar stock \n3:Borrar Producto \n4:Descontar Stock \n5:Calcular Cuotas"))){
+function menuEleccion(menu = parseInt(prompt("Ingrese que quiere realizar: \n1:Agregar Producto \n2:Agregar stock \n3:Borrar Producto \n4:Descontar Stock "))){
     switch (menu){
         case 1:
             agregarProduto(productos)
             alert("Se agrego el producto correctamente")
+            if (confirm("desea Realizar otra operacion?")){
+                menuEleccion()
+            }
             break
         case 2:
             agregarStock(productos)
             alert("Se modifico el stock correctamente")
+            if (confirm("desea Realizar otra operacion?")){
+                menuEleccion()
+            }
             break
         case 3:
             productos=borrarProducto(productos)
             alert("Se elimino el producto correctamente")
+            if (confirm("desea Realizar otra operacion?")){
+                menuEleccion()
+            }
             break
         case 4:
             disminuirStock(productos)
-            alert("Se modifico el stock correctamente")
-            const a= confirm("desea Realizar otra operacion?")
-            if (a){
+            alert("Se modifico el stock correctamente")             
+            if (confirm("desea Realizar otra operacion?")){
                 menuEleccion()
             }
             break
         
         default:
-            alert("Opcion incorrecta, elegir opcion valida")  
-            
-            menuEleccion(menu = parseInt(prompt("Ingrese que quiere realizar: \n1:Agregar Producto \n2:Agregar stock \n3:Borrar Producto \n4:Descontar Stock \n5:Calcular Cuotas")))
-    
+            alert("Opcion incorrecta, elegir opcion valida")              
+            menuEleccion()
     }
 }
-
-
 class Producto {
     constructor(nombre, tipo, valor, color,stock){
         this.nombre = nombre.toUpperCase();
@@ -119,19 +118,13 @@ class Producto {
     sumarIva(){
         this.valor=this.valor* iva
     }
-
 }
-let productos=[]
-const iva= 1.21
 
+//------------------------------------------------------------------------------------------------
+let productos=[]
 productos.push(new Producto ("Remera con estampa", "1", "500","negra","9"),new Producto ("Remera lisa", "1", "300","blanca","5"),new Producto ("Jean skinny", "2", "1200","azul", "2"))
 
-
-//const menu = parseInt(prompt("Ingrese que quiere realizar: \n1:Agregar Producto \n2:Agregar stock \n3:Borrar Producto \n4:Descontar Stock \n5:Calcular Cuotas"))
-
-
-
-
+menuEleccion()
 
 
 
